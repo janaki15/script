e6bb4f3bfdc7c12d1cb7ac7242c500116ca69554
janaki15 <bhavanigouti123@gmail.com>
2019-09-02 09:12:55 +0530

    aj

---
+++ details.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "THIS SCRIPT WILL PROVIDE INFORMATION ABOUT THE LOCAL SYSTEM"
+echo
+#GIVES HOSTNAME
+echo $(hostname)
+echo
+#GIVES USER EXECUTED THIS SCRIPT
+echo $(whoami)
+echo
+#GIVES USERS IN THE SYSTEM
+USERS=$(cat /etc/passwd | cut -d ":" -f 1)
+echo $USERS
+echo
+#GET IP ADDRES OF THE SYSTEM
+IP=$(ifconfig)
+echo $IP
